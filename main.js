e7dbe89710094fe46abf79a73a34faba3aeec219
unknown <gibmac@outlook.com>
2022-07-29 09:51:55 -0600

    css before sass backup

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const navigation = document.querySelector("nav");
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 function navBurger() {
     if (navigation.className === "toggled") {
@@ -8,14 +9,27 @@ function navBurger() {
     }
 }
 
-(function typer(){
-    let cont = '';
-    let index = 0;
-    const elem = document.querySelector('.type')
-    currentText = elem.getAttribute('texCon');
-    cont = currentText.slice(0,++index);
-    elem.textContent = cont;
+( async function typer() {
+    await timer(1000);
 
-    setTimeout(typer, 400);
-    
+    let cont = ''
+    let index = 0;
+    const typers = document.querySelectorAll('.type');
+    for (let j = 0; j < typers.length; j++) {
+        elem = typers[j]
+        currentText = elem.getAttribute('texCon');
+        elem.classList.add('typing');
+        for (let i = 0; i < currentText.length; i++) {
+            cont = currentText.slice(0,++index);
+            elem.textContent = cont + "|";
+            await timer(Math.random() * 150);
+        }
+        elem.textContent = cont
+        index = 0;
+        elem.classList.remove('typing');
+        if (j == typers.length-1) {
+            elem.classList.add('typedone');
+            console.log(j);
+        }
+    }
 }());
